a867e653a45a913e3dd11a8766e2cd6bb601e90a
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-08-21 10:25:06 +0200

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,90 @@
+var phones; // global variable used by both functions: createPhonese & displayPhones;
+
+function Phone(brand, model, price, color, screenSize, resolH, resolV) {
+    this.brand = brand;
+    this.model = model;
+    this.price = price;
+    this.color = color;
+    this.screenSize = screenSize;
+    this.resolH = resolH;
+    this.resolV = resolV;
+}
+
+Phone.prototype.printInfo = function() {
+
+var model1 = document.getElementById('model-1'),
+    model2 = document.getElementById('model-2'),
+    model3 = document.getElementById('model-3'),
+    phoneDetails;
+
+switch (this.model) {
+    case "Samsung Galaxy S6":
+    model1.innerText = this.model;
+    phoneDetails = `Brand: ${this.brand};\nColour: ${this.color};\n Screen size: ${this.screenSize}";\nResolution: ${this.resolH}x${this.resolV} pixels;\n Price: ${this.price} PLN.`;
+    return phoneDetails;
+    break;
+
+    case "iPhone 6S":
+    model2.innerText = this.model;
+    phoneDetails = `The phone has been produced by ${this.brand},\n it is ${this.color}, the screen size is ${this.screenSize}",
+the resolution is ${this.resolH}x${this.resolV} pixels. It costs ${this.price}PLN.`;
+    return phoneDetails;
+    break;
+
+    case "OnePlus One":
+    model3.innerText = this.model;
+    phoneDetails = `The phone has been produced by ${this.brand},\n it is ${this.color}, the screen size is ${this.screenSize}",
+the resolution is ${this.resolH}x${this.resolV} pixels. It costs ${this.price}PLN.`;
+    return phoneDetails;
+    break;
+
+    default:
+    alert("Something's not right");
+}
+}
+
+// methods:
+Phone.prototype.ring = function() {
+    console.log(`${this.model} is ringing`);
+}
+
+Phone.prototype.sendText = function() {
+
+}
+
+Phone.prototype.takePhoto = function() {
+
+}
+
+// ***************************************************************************************************
+
+function createPhones() {
+
+var samsungGalaxyS6 = new Phone("Samsung Electronics", "Samsung Galaxy S6", 2250, "black", 5.5, 1440, 2560),
+    iPhone6S = new Phone("Apple", "iPhone 6S", 1750, "silver", 4.7, 750, 1334),
+    onePlusOne = new Phone("OnePlus", "OnePlus One", 1950, "white", 5.5, 1080, 1920);
+
+    phones = [samsungGalaxyS6, iPhone6S, onePlusOne];
+
+// ---------------------------------------------------------------------------------------------------
+}
+
+function displayPhones() {
+var i = 0,
+    infoText,
+    phonesLength = phones.length;
+
+while (i < phonesLength) {
+    infoText = phones[i].printInfo();
+    document.getElementById(`info-${i+1}`).innerText = infoText;
+    i++;
+}
+}
+
+// ***************************************************************************************************
+
+createPhones();
+displayPhones();
+
+
+
