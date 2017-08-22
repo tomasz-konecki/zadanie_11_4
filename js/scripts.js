056b3663b6db7af14bf0fa3386161afdbd8983d5
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-08-22 16:20:17 +0200

    Correct var declarations

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
-var phones,
-    btnRing = document.getElementsByClassName('btn-ring'),
+var btnRing = document.getElementsByClassName('btn-ring'),
     btnText = document.getElementsByClassName('btn-text'),
     btnPhoto = document.getElementsByClassName('btn-photo'),
-    paragraphs = document.getElementsByTagName('p');
+    paragraphs = document.getElementsByTagName('p'),
 
+    samsungGalaxyS6 = new Phone("Samsung Electronics", "Samsung Galaxy S6", 2250, "black", 5.5, 1440, 2560),
+    iPhone6S = new Phone("Apple", "iPhone 6S", 1750, "silver", 4.7, 750, 1334),
+    onePlusOne = new Phone("OnePlus", "OnePlus One", 1950, "white", 5.5, 1080, 1920),
+
+    phones = [samsungGalaxyS6, iPhone6S, onePlusOne];
 // ---------------------------------------------------------------------------------------------------
 
 function Phone(brand, model, price, color, screenSize, resolH, resolV) {
@@ -15,8 +19,6 @@ function Phone(brand, model, price, color, screenSize, resolH, resolV) {
     this.resolH = resolH;
     this.resolV = resolV;
 }
-
-// ---------------------------------------------------------------------------------------------------
 
 Phone.prototype.printInfo = function() {
     return `Brand: ${this.brand};\nColour: ${this.color};\n Screen size: ${this.screenSize}";\nResolution: ${this.resolH}x${this.resolV} pixels;\n Price: ${this.price} PLN.`;
@@ -36,14 +38,6 @@ Phone.prototype.takePhoto = function(n) {
     paragraphs[n].innerText = `${this.model} is taking a photo!`;
     resetAction(n);
 }
-
-// ---------------------------------------------------------------------------------------------------
-
-var samsungGalaxyS6 = new Phone("Samsung Electronics", "Samsung Galaxy S6", 2250, "black", 5.5, 1440, 2560),
-    iPhone6S = new Phone("Apple", "iPhone 6S", 1750, "silver", 4.7, 750, 1334),
-    onePlusOne = new Phone("OnePlus", "OnePlus One", 1950, "white", 5.5, 1080, 1920);
-
-phones = [samsungGalaxyS6, iPhone6S, onePlusOne];
 
 // ---------------------------------------------------------------------------------------------------
 
